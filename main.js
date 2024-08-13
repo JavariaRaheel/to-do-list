387009703e179e7281edc030de6db58665dc7741
JavariaRaheel <168272128+JavariaRaheel@users.noreply.github.com>
2024-08-13 13:08:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+import { input } from '@inquirer/prompts';
+import { confirm } from '@inquirer/prompts';
+import chalk from "chalk";
+//make arry todo list
+let todoList = [];
+console.log(chalk.magentaBright.bold("\n \t Welcome to our TODOLIST app\n"));
+let conditions = true;
+while (conditions) {
+    let addTask = await input({
+        message: chalk.redBright("Enter your New task:")
+    });
+    todoList.push(addTask);
+    console.log(`${addTask}`);
+    let addMoreTask = await confirm({
+        message: chalk.green("DO you want to add more task?"),
+    });
+    conditions = addMoreTask;
+}
+console.log("your updated TodoList", todoList);
